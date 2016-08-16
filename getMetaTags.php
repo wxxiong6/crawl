13a2f9bbda5e6e6a4f66c9d361dfa1e39762db95
vip6111111 <601115211@qq.com>
2016-08-16 17:41:26 +0800

    修改信息
    修改信息

--- getMetaTags.php
+++ getMetaTags.php
@@ -1,11 +1,4 @@
 <?php
-$url = "http://www.sina.com.cn";
-$str = file_get_contents($url);
-//echo $str;
-//$str = file_get_contents('3.html');
-
-
-
 /**
  *  从一个字符串提取所有的 meta 标签 content 属性，返回一个数组
  *  get_meta_tags
@@ -15,15 +8,12 @@ function getMetaTags($str)
     $result = array();
     $matePattern = '/<meta\s+(content|name\s?=.*)\s?\/?>/iUs';
     preg_match_all($matePattern, $str, $mateMatch);
-//    print_r($mateMatch);exit;
     if(count($mateMatch) > 0){
          foreach($mateMatch[1] as $v){
              $v = $v." ";
              $matePatternReplace = '/(name|content)\s?=\s?(["\S"]+|[\S]+)\s+/iUs';
              preg_match_all($matePatternReplace, $v, $mateMatchReplace);
-//             print_r($mateMatchReplace);
              if(count($mateMatchReplace) > 0){
-//                 print_r($mateMatchReplace);
                  if(!empty($mateMatchReplace[2][0])) {
                      if(empty($mateMatchReplace[2][1])) $mateMatchReplace[2][1] = '';
                      else
@@ -38,8 +28,5 @@ function getMetaTags($str)
 }
 
 
-print_r(getMetaTags($str));
-//$metas = get_meta_tags($url);
-//print_r($metas);
 
 
