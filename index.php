0d8fa26d1532a7f42f27c0c66902cfe8e235d6de
vip6111111 <601115211@qq.com>
2016-09-03 10:24:22 +0800

    增加数据库名称配置项

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ define('LIB_PATH', ROOT_PATH . 'code/library' . DIRECTORY_SEPARATOR);
 defined('LOG_PATH') or define('LOG_PATH', ROOT_PATH . 'log' . DIRECTORY_SEPARATOR);
 defined('TEP_PATH') or define('TEP_PATH', ROOT_PATH . 'tmp' . DIRECTORY_SEPARATOR);
 defined('DATA_PATH') or define('DATA_PATH', ROOT_PATH . 'data' . DIRECTORY_SEPARATOR);
+defined('DB_NAME') or define('DB_NAME', 'crawl');
+
 // 环境常量
 define('IS_CLI', PHP_SAPI == 'cli' ? true : false);
 define('IS_WIN', strpos(PHP_OS, 'WIN') !== false);
