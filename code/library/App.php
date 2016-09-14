6cb47d32f5f336581e22d9c2dd7c03109cb16e45
vip6111111 <601115211@qq.com>
2016-09-14 10:21:44 +0800

    增加run 方法

--- code/library/App.php
+++ code/library/App.php
@@ -3,8 +3,18 @@ namespace library;
 
 class App
 {
-
-    public static function run()
-    {}
+    /**
+     * 执行程序
+     * @param object $import
+     * @param array $argv
+     */
+    public static function run($import, array $argv = [])
+    {
+        if( ! method_exists($import, $argv[1])){
+            exit('error');
+        }
+        if (!isset($argv[2])) $argv[2] = '';
+        call_user_func(array($import, $argv[1]) , $argv[2]);
+    }
 }
 
